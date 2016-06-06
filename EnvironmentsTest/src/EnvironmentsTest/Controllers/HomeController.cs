93761b09246208e85db7e60a5736bbc5f2df343f
Hernan Guzman Rendon <hernandgr@gmail.com>
2016-06-06 12:23:56 -0500

    Multiple environments test

---
+++ EnvironmentsTest/src/EnvironmentsTest/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnvironmentsTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IHostingEnvironment _environment;
+
+        public HomeController(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
+        public IActionResult Index()
+        {
+            if (_environment.IsDevelopment())
+            {
+                // ASPNET_ENV = Development
+                ViewData["environment"] = "Desarrollo";
+            }
+            else if (_environment.IsStaging())
+            {
+                // ASPNET_ENV = Staging
+                ViewData["environment"] = "Pruebas - Staging";
+            }
+            else if (_environment.IsProduction())
+            {
+                // ASPNET_ENV = Production
+                ViewData["environment"] = "Produccion";
+            }
+            else if (_environment.IsEnvironment("MiAmbiente"))
+            {
+                // ASPNET_ENV = MiAmbiente
+                ViewData["environment"] = "Mi ambiente";
+            }
+            else
+            {
+                // ASPNET_ENV = ?
+                ViewData["environment"] = "Otro ambiente :P";
+            }
+
+            return View();
+        }
+    }
+}
